62053c2183a46c556ba359c4135b0cfc8eef1dbd
Jérémy Laurens <jeremy.laurens@Digital-Grenoble.local>
2018-06-13 10:20:59 +0200

    tbody generateCases OK

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 var grille = new Array();
 var theme = new Array();
 theme = [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
@@ -11,22 +13,38 @@ for(var i=0; i<4; i++){
     console.log(indexTheme + " " + theme[indexTheme]);
     indexTheme++;
   }
-  generateCases();
 }
 
+$("button").click(function(){
+      generateCases();
+});
 
 
 
 function generateCases(){
 	console.log("test");
-	$( ".container" ).html(function(){
-		while (n < theme.length) {
+	var n = 0;
+	var x = 0;
+	var myCases = "";
+	while ( n < theme.length-1) {
+		
+		myCases = myCases + "<tr>";
+		while(x < 4){
+			var myCases = myCases + "<td>" + theme[n] + "</td>";
 			n++;
-			var my = "<td>" + theme[n] + "</td>";
+			x++;
 		}
-		my = "<td>" + theme[0] + "</td>";
-		return "<tr>All new content for " + my + "</tr>";
-	});
+		x = 0;
+		myCases = myCases + "</tr>";
+	}
 
+
+
+	$( "tbody" ).html(myCases);
+	
 	return true;
 }
+
+
+
+});
